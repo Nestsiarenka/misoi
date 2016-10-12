949740f249d271b406e783d784fc71e63e0b04fb
Vadzim Nestsiarenka <westerte@gmail.com>
2016-10-12 15:26:54 +0300

    Converter now works correctly!!!!!!

--- ImageProcessingLibrary/ImageProcessingLibrary/Utilities/Converter.cs
+++ ImageProcessingLibrary/ImageProcessingLibrary/Utilities/Converter.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using ImageProcessingLibrary.Capacities.Interface;
 using ImageProcessingLibrary.Capacities.Structures;
-using ImageProcessingLibrary.Exceptions;
 using ImageProcessingLibrary.Images;
 
 namespace ImageProcessingLibrary.Utilities
 {
     public class Converter
     {
-        public delegate void BitmapInitializator<T>(int i, int j, T pixel, byte[] array) where T: struct, ICapacity;
+        public delegate void BitmapInitializator<T>(int c, T pixel, byte[] array) where T: struct, ICapacity;
 
         public static Image<RGB> ToImage(Bitmap bitmap) 
         {
@@ -33,40 +31,28 @@ namespace ImageProcessingLibrary.Utilities
         }
 
         private static Bitmap InitializeBitmapFromImage<T> (BitmapInitializator<T> initializator, 
-            Image<T> image, PixelFormat pixelFormat = PixelFormat.Format32bppRgb)
+            Image<T> image, PixelFormat pixelFormat = PixelFormat.Format24bppRgb)
             where T: struct, ICapacity
         {
             int n = image.N;
             int m = image.M;
 
-            byte[] imageLikeByteArray;
-            int stride;
-
-            switch (pixelFormat)
-            {
-                case PixelFormat.Format32bppRgb:
-                    imageLikeByteArray = new byte[n * m * 4];
-                    stride = n * 4;
-                    break;
-                case PixelFormat.Format8bppIndexed:
-                    imageLikeByteArray = new byte[n * m];
-                    stride = n;
-                    break;
-                default:
-                    throw new ConverterException("Unsupported PixelFormat type");
-            }
-
-            for (int j = 0; j < m; j++)
-            {
-                for (int i = 0; i < n ; i++)
-                {
-                    initializator(i, j, image[i, j], imageLikeByteArray);
-                }
-            }
-            
             var bitmap = new Bitmap(n, m, pixelFormat);
             var data = bitmap.LockBits(new Rectangle(0, 0, n, m),
                 ImageLockMode.WriteOnly, pixelFormat);
+
+            byte[] imageLikeByteArray;
+            int stride = data.Stride;
+
+            imageLikeByteArray = new byte[m * stride];
+
+            for (int j = 0; j < m; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    initializator(j * stride + i * (stride/n), image[i, j], imageLikeByteArray);
+                }
+            }
 
             Marshal.Copy(imageLikeByteArray, 0, data.Scan0, imageLikeByteArray.Length);
 
@@ -76,22 +62,27 @@ namespace ImageProcessingLibrary.Utilities
 
         public static Bitmap ToBitmap(Image<RGB> image)
         {
-            return InitializeBitmapFromImage((i, j, pixel, array) =>
+            return InitializeBitmapFromImage((c, pixel, array) =>
             {
-                var currentIndex = (j * image.N + i) * 4;
-                array[currentIndex] = (byte)pixel.B;
-                array[currentIndex + 1] = (byte)pixel.G;
-                array[currentIndex + 2] = (byte)pixel.R;
+                array[c] = (byte)pixel.B;
+                array[c + 1] = (byte)pixel.G;
+                array[c + 2] = (byte)pixel.R;
             }, image);
         }
 
         public static Bitmap ToBitmap(Image<Gray> image)
         {
-            return InitializeBitmapFromImage((i, j, pixel, array) =>
+            Bitmap bitmap =  InitializeBitmapFromImage((c, pixel, array) =>
             {
-               var currentIndex = j * image.N + i;
-               array[currentIndex] = (byte)pixel.G;
+               array[c] = (byte)pixel.G;
             }, image, PixelFormat.Format8bppIndexed);
+
+            ColorPalette pal = bitmap.Palette;
+            for (int i = 0; i < 256; i++)
+                pal.Entries[i] = Color.FromArgb(255, i, i, i);
+            bitmap.Palette = pal;
+
+            return bitmap;
         }
     }
 }
